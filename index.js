59b836505ae647a0c2c537315fe347d86640bf5d
Parth Seth <parthseth2004@gmail.com>
2024-06-08 00:59:07 +0530

    Init Commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const b = document.querySelector(".joinButton");
+if(b)
+b.addEventListener("click",()=>{
+    window.location.href = 'index2.html';
+});
+
+var canvas = document.querySelector(".canvas");
+    var context = canvas.getContext('2d');
+    
+    
+    var isDrawing = false;
+    var lastX = 0;
+    var lastY = 0;
+
+    function drawLine(x1, y1, x2, y2) {
+        context.beginPath();
+        context.moveTo(x1, y1);
+        context.lineTo(x2, y2);
+        context.stroke();
+        context.closePath();
+    }
+
+    
+    canvas.addEventListener('mousedown', function(e) {
+        isDrawing = true;
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+    });
+
+    
+    canvas.addEventListener('mousemove', function(e) {
+        if (isDrawing) {
+            drawLine(lastX, lastY, e.offsetX, e.offsetY);
+            [lastX, lastY] = [e.offsetX, e.offsetY];
+        }
+    });
+
+    
+    canvas.addEventListener('mouseup', function() {
+        isDrawing = false;
+    });
+
+    
+    canvas.addEventListener('mouseleave', function() {
+        isDrawing = false;
+    });
